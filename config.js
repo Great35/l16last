be360f43fe44af52ef84939aab185a49104f1b0a
Great35 <Greatitodo35@gmail.com>
2025-04-10 15:15:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 export const config = {
-    botToken: '7879454224:AAGb1Q9OY06x0MOUBBGHBExPZEqKb6Gg-R0',
+    botToken: '7308738484:AAFmyqBznahHExUlAWtfzxkrLVUZs1fI8rM',
     mongoURI: 'mongodb+srv://luna:chidera1515@luna.6vhbamm.mongodb.net/meritDB?retryWrites=true&w=majority'
 };
